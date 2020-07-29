c1e5fa29781ee85b6054fa1cf0481f9b1ae26c2b
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-07-29 15:36:21 +0700

    Upload files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Button } from '@material-ui/core';
 import './App.css';
+import years from './years';
 
-function App() {
+const App = () => {
+  const hint = 'Hover over any entity to display its name here.';
+  const [year, setYear] = useState(-1);
+  const [entity, setEntity] = useState(hint);
+
+  const handleApiLoaded = (map, maps) => {
+    const yearChosen = year > 0 ? year : `bc${-year}`;
+    map.data.loadGeoJson(
+      `https://raw.githubusercontent.com/aourednik/historical-basemaps/master/world_${yearChosen}.geojson`
+    );
+    map.data.addListener('click', (e) => {
+      setEntity(e.feature.getProperty('NAME'));
+    });
+    map.data.addListener('mouseover', (e) => {
+      setEntity(e.feature.getProperty('NAME'));
+    });
+    map.data.addListener('mouseout', () => {
+      setEntity(hint);
+    });
+    console.log(process.env.REACT_APP_GOOGLE_MAPS_API_KEY);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className='top'>
+        <h1>ChronoMap</h1>
+        <p>Current Year: {year > 0 ? `${year} CE` : `${-year} BCE`}</p>
+        <div>
+          {years.map((year) => (
+            <Button
+              variant='outlined'
+              key={year}
+              color='primary'
+              onClick={() => setYear(year)}
+            >
+              {year}
+            </Button>
+          ))}
+        </div>
+        <p>{entity}</p>
+      </div>
+      <div className='map'>
+        <GoogleMapReact
+          key={year}
+          bootstrapURLKeys={{ key: 'AIzaSyCv2GYlbEmB0D2wHcc55phmBHWmtzKvQMI' }}
+          defaultCenter={{ lat: 20, lng: 0 }}
+          defaultZoom={1}
+          yesIWantToUseGoogleMapApiInternals
+          onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps)}
+        />
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
