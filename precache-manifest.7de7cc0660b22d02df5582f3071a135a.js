537b27ce5bb22a218889bbfe2d574cd7573337ee
ArchawinWongkittiruk <willypoko@hotmail.com>
2020-07-29 15:37:45 +0700

    Updates

---
+++ precache-manifest.7de7cc0660b22d02df5582f3071a135a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb2528c1f91cae7db9c3ccbf4471efec",
+    "url": "/ChronoMap/index.html"
+  },
+  {
+    "revision": "4be712d30683b1149708",
+    "url": "/ChronoMap/static/css/main.ed5c9d01.chunk.css"
+  },
+  {
+    "revision": "7f62002718ae09a9096b",
+    "url": "/ChronoMap/static/js/2.7190d2e6.chunk.js"
+  },
+  {
+    "revision": "3a08a7c8f8e6231522abe29a929e6912",
+    "url": "/ChronoMap/static/js/2.7190d2e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4be712d30683b1149708",
+    "url": "/ChronoMap/static/js/main.e4aa2ba1.chunk.js"
+  },
+  {
+    "revision": "7ea55573e368ef49fb27",
+    "url": "/ChronoMap/static/js/runtime-main.96c54e0c.js"
+  }
+]);
